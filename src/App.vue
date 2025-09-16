3e4baf05268ea21176eb3da808807f8df82b1f7a
Aprilla Daru <darupangestu3000@gmail.com>
2025-09-16 21:18:41 +0700

    src

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <DestinationExplorer />
+  </div>
+</template>
+
+<script>
+import DestinationExplorer from './components/DestinationExplorer.vue'
+export default {
+  name: 'App',
+  components: { DestinationExplorer }
+}
+</script>
+
+<style>
+body { font-family: Inter, ui-sans-serif, system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial; }
+</style>
